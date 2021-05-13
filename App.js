23b16d65a2f7ecc293aad112f04521c2d9044886
Adrian <adriann.kowalski@gmail.com>
2021-05-13 20:12:21 +0200

    display fetched data on FlatList

--- App.js
+++ App.js
@@ -2,30 +2,21 @@ import React, { useState, useEffect } from 'react';
 import RoomList from './Components/RoomList';
 import { Rooms } from './Components/queries';
 
-const DATA = [
-  {
-    title: "Rooms",
-    data: ["Room1", "Room2", "Room3"]
-  }
-];
-
 const App = () => {
 
-const [fetchedRooms, setFetchedRooms] = useState({})
+const [fetchedRooms, setFetchedRooms] = useState([])
 
 const getData = async () => {
   let data = await Rooms()
-  setFetchedRooms(data)
+  setFetchedRooms(data.response)
   }
 
 useEffect(()=> {
 getData()
 }, [])
 
-console.log(fetchedRooms)
-
 return (
-<RoomList data={DATA}
+<RoomList data={fetchedRooms}
   />
 )};
 
--- Components/RoomList.js
+++ Components/RoomList.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import RoomItem from '../Components/RoomItem'
-import { Text, SafeAreaView, SectionList } from 'react-native';
+import { Text, SafeAreaView, FlatList, View } from 'react-native';
 import styles from '../styles/styles';
 
 
+const RoomList = ({ data }) => {
 
-const RoomList = ({ data }) => (
+  const renderItem = ({ item }) => (
+    <Item title={item.name} />
+  );
+
+  const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  )
+
+  return (
     <SafeAreaView style={styles.container}>
-      <SectionList
-        sections={data}
-        keyExtractor={(item, index) => item + index}
-        renderItem={({ item }) => <RoomItem title={item} />}
-        renderSectionHeader={({ section: { title } }) => (
-        <Text style={styles.header}>{title}</Text>
-        )}
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
       />
     </SafeAreaView>
   );
+}
 
-  export default RoomList;
+export default RoomList;
