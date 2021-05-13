8e7db0fea79262a02b8539338d42b3eb8653b8ea
Adrian <adriann.kowalski@gmail.com>
2021-05-13 10:24:00 +0200

    move styles from App.js to external file

---
+++ styles/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '50vw'
+    },
+    text: {
+      fontSize: '50px'
+    }
+  });
+
+  export default styles;
